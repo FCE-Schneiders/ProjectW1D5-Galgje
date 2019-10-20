5be900ac0fac915520bf093fa5566891f6016ee1
Fleur <fleur.schneiders@live.nl>
2019-10-20 21:53:43 +0200

    bijna af

--- Galgje.js
+++ Galgje.js
@@ -1,38 +1,98 @@
-//invoer veld input werkend maken  link inzetten.
-// random woorden waaruit gekozen kan woorden
-let wordsArray = ["apple", "soccer", "castle","pillow", "letter"]
-let randomNumber =Math.floor( Math.random() * wordsArray.length);
-console.log(randomNumber) //om te controleren 
-console.log(Math.floor(randomNumber)) 
-
-
-// je wilt een randomword (op index 0,1,2,3,4) uit de wordsarray halen => randomNumber
-// let randomWord = Math.random() * 
-//let randomWord = wordsArray[0 of 1 of 2 of 3 of 4] is het zelfde als wordsArray [randomNumber]
-let randomWord = wordsArray [randomNumber]
-console.log(randomWord) // voor mezelf om te checken
-
-//het random woord opsplitsen in aparte karakters
-// gebruik de splitmethod.
+let lives = 5;
+let conent = '';
+let wordsArray = ["apple", "presidential", "weesp", "pillow", "Amsterdam"];
+let randomNumber = Math.floor(Math.random() * wordsArray.length);
+let randomWord = (wordsArray[randomNumber]).toLowerCase();
 let arrayRandomWord = randomWord.split("");
-console.log(arrayRandomWord.length) //even voor de check
+let numberOfCharacters = arrayRandomWord.length;
+let wordToGuess = '';
+let nieuweArryWord = [];
 
-// functies
 
-function checkGuess(){
-    console.log(document.getElementById("myText").value);
+for (var i = 0; i < arrayRandomWord.length; i++) { nieuweArryWord[i] = " _ "; }
+
+
+
+
+function fillInGuessdWord() {
+  for (let index = 0; index < numberOfCharacters; index++) {
+    consoleLog(guessedCharacters[index]);
+
+  }
 }
-function restartGame(){
-    location.reload();
+
+function printHtmlCode(classOrId, content) {
+  document.querySelector(classOrId).innerHTML = content;
+
+}
+function consoleLog(valueToPrint) {
+  console.log(valueToPrint);
+}
+
+function livesRemaining() {
+  printHtmlCode("#totalLives", 'You still have  ' + lives + ' turns left!');
+
+}
+
+function printWordToGuessUit(numberOfCharacters) {
+  printHtmlCode('#word', numberOfCharacters);
+}
+
+function startGuessGame() {
+  livesRemaining();
+  consoleLog(randomWord);
+  let currentWord = '';
+  for (var i = 0; i < arrayRandomWord.length; i++) {
+    nieuweArryWord[i] = " _ ";
+    currentWord += nieuweArryWord[i];
   }
-    
-  //onderstaande uitwerken nog.
-  let lives //aantal guesses - guess is een herhaling.
+  printHtmlCode('#allready-guessed', currentWord);
+}
 
-function livesRemaining (){
-      document.getElementById("totalLives").innerHTML='5' // moderne manier is querySelector even aanpassen 
-                                                          //hoe ga ik de variabele (levens) veranderen?
 
+function restartGame() {
+  location.reload(true);
+}
+
+
+function checkGuess() {
+  let guessInputCharacter = document.querySelector('#inputGuess').value;
+  if (arrayRandomWord.includes(guessInputCharacter)) {
+
+  } else {
+    lives--;
   }
 
+  if (lives > 0 & nieuweArryWord.includes(' _ ')) {
+
+    for (var i = 0; i < arrayRandomWord.length; i++) {
+      if (arrayRandomWord[i] === guessInputCharacter) {
+        nieuweArryWord[i] = guessInputCharacter;
+      } else {
+
+      }
+
+    }
+
+  }
+  else if (!nieuweArryWord.includes(' _ ')) {
+    alert('u heeft gewonnen');
+  }
+  else {
+    alert('Game over, probeer opnieuw! Druk op OK');
+    restartGame();
+  }
+
+
+  let currentWord = '';
+  for (var i = 0; i < arrayRandomWord.length; i++) {
+    currentWord += nieuweArryWord[i];
+  }
+  printHtmlCode('#allready-guessed', currentWord)
+  livesRemaining();
+
+}
+
+
+startGuessGame();
 
